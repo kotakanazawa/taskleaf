c3de612dc880c0a502daa86f785abf09499602df
Kota Kanazawa <kotakanazawa@Kohs-MBP.local>
2020-01-29 11:02:28 +0900

    first commit

---
+++ app/models/task.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+  def validate_name_not_including_comma
+    errors.add(:name, "にカンマを含めることはできません") if name&.include?(",")
+  end
+end
